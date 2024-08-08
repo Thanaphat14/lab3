dc25c52a5f99710389ead429b46cf609ba7034e8
Thanaphat Sanngoen <blazesonic14@gmail.com>
2024-08-08 17:53:56 +0700

    5.2 commit

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -7,6 +7,7 @@ import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -57,16 +58,24 @@ const router = createRouter({
 
     },
     {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
+    },
+    
+    {
       path: '/:catchAll(.*)',
       name: 'not-found',
       component: NotFoundView
 
     },
+    
     {
       path: '/student',
       name: 'student',
       component: StudentView
-    }
+    },
+    
   ]
 })
 
