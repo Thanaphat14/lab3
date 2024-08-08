4af91d51cfb2f1a4d4dd364d1dcb18b73772d987
Thanaphat Sanngoen <blazesonic14@gmail.com>
2024-08-08 17:30:37 +0700

    5.1 commit

--- 331-intro-to-vite/src/views/event/LayoutView.vue
+++ 331-intro-to-vite/src/views/event/LayoutView.vue
@@ -2,6 +2,9 @@
 import { ref, onMounted } from 'vue'
 import { type Event } from '@/types'
 import EventService from '@/services/EventService'
+import { useRouter } from 'vue-router'
+import { resourceLimits } from 'worker_threads';
+
 
 const event = ref<Event | null>(null)
 const props = defineProps({
@@ -10,13 +13,18 @@ const props = defineProps({
     required: true
   }
 })
+const router = useRouter()
+
 onMounted(() => {
   EventService.getEvent(parseInt(props.id))
     .then((response) => {
       event.value = response.data
     })
-    .catch((error) => {
-      console.error('There was an error!', error)
+    .catch(() => {
+      router.push({
+        name: '404-resource-view',
+        params: {resource: 'event'}
+      })
     })
 })
 </script>
